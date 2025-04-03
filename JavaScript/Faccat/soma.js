3ace265718470a99e4551d5ab1995b5ef864de75
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-03 12:17:05 -0300

    Delete JavaScript/Faccat/soma.js

--- JavaScript/Faccat/soma.js
+++
@@ -1,4 +0,0 @@
-let numero = parseInt(prompt("Digite um valor: "));
-let numero1 = parseInt(prompt("Digite um outro valor: "));
-let soma = numero + numero1;
-alert("A soma dos números digitados é: " + soma);
