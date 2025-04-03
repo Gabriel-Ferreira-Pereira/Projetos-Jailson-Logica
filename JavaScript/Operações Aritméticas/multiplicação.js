9f802bea589f9178a82a5fa0a3df4b119f8819f9
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-03 12:15:26 -0300

    Add files via upload

---
+++ JavaScript/Operações Aritméticas/multiplicação.js
@@ -0,0 +1,4 @@
+let numero = parseInt(prompt("Digite um valor: "));
+let numero1 = parseInt(prompt("Digite um outro valor: "));
+let multiplicacao = numero * numero1;
+alert("O resultado da multiplicação dos valores é: " + multiplicacao + ".");
---
+++ JavaScript/Operações Aritméticas/subtração.js
@@ -0,0 +1,4 @@
+let numero = parseInt(prompt("Digite um valor: "));
+let numero1 = parseInt(prompt("Digite um outro valor: "));
+let subtracao = numero - numero1;
+alert("O resultado da subtração dos valores é :" + subtracao + ".");
