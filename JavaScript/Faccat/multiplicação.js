93d20e541d433a9c389412e8abbff77a0634b7b6
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-03 12:16:57 -0300

    Delete JavaScript/Faccat/multiplicação.js

--- JavaScript/Faccat/multiplicação.js
+++
@@ -1,4 +0,0 @@
-let numero = parseInt(prompt("Digite um valor: "));
-let numero1 = parseInt(prompt("Digite um outro valor: "));
-let multiplicacao = numero * numero1;
-alert("O resultado da multiplicação dos valores é: " + multiplicacao + ".");
