50a8ab15dead4d05074001b757595e8679b9906b
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-03 12:16:47 -0300

    Delete JavaScript/Faccat/divisão.js

--- JavaScript/Faccat/divisão.js
+++
@@ -1,4 +0,0 @@
-let numero = parseInt(prompt("Digite um valor: "));
-let numero1 = parseInt(prompt("Digite o segundo valor:"));
-let divisao = numero / numero1;
-alert("O resultado da divisão é " + divisao + ".");
