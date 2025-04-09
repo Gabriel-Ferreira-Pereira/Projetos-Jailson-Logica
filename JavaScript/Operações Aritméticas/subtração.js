b15dcc50b8d19b14ce16acd6475039967e38625b
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-09 19:59:55 -0300

    Delete JavaScript/Operações Aritméticas/subtração.js

--- JavaScript/Operações Aritméticas/subtração.js
+++
@@ -1,4 +0,0 @@
-let numero = parseInt(prompt("Digite um valor: "));
-let numero1 = parseInt(prompt("Digite um outro valor: "));
-let subtracao = numero - numero1;
-alert("O resultado da subtração dos valores é :" + subtracao + ".");
