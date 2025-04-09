8f41215c0c7cd9740096a6eaf8239634d003c0fc
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-09 20:09:30 -0300

    Add files via upload

---
+++ JavaScript/Operações Aritméticas/Divisão/divisão.js
@@ -0,0 +1,5 @@
+alert("Divisão");
+let numero1 = parseFloat(prompt("Digite um número: "));
+let numero2 = parseFloat(prompt("Digite um número: "));
+let divisao = numero1 / numero2;
+alert(numero1 + " / " + numero2 + " = " + divisao);
