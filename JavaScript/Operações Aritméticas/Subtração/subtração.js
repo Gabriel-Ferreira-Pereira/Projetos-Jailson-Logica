7e8efc479357b26f2f1ca3b3c51bdd1b757e08fc
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-09 20:04:29 -0300

    Add files via upload

---
+++ JavaScript/Operações Aritméticas/Subtração/subtração.js
@@ -0,0 +1,5 @@
+alert("Subtração");
+let numero1 = parseFloat(prompt("Digite um número: "));
+let numero2 = parseFloat(prompt("Digite um número: "));
+let subtração = numero1 - numero2;
+alert(numero1 + " - " + numero2 + " = " + subtração);
