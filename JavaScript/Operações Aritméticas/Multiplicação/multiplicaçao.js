b34343e72d6ff72ea417476b038bf584b8b529f4
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-09 20:06:54 -0300

    Add files via upload

---
+++ JavaScript/Operações Aritméticas/Multiplicação/multiplicaçao.js
@@ -0,0 +1,5 @@
+alert("Multiplicação");
+let numero1 = parseFloat(prompt("Digite um número: "));
+let numero2 = parseFloat(prompt("Digite um número: "));
+let multiplicacao = numero1 * numero2;
+alert(numero1 + " X " + numero2 + " = " + multiplicacao);
