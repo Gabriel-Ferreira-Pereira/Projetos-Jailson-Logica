b8897179330de1a9eacda3127bb1543618c1c183
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-04-09 20:00:41 -0300

    Add files via upload

---
+++ JavaScript/Operações Aritméticas/Soma/soma.js
@@ -0,0 +1,5 @@
+alert("Soma");
+let numero1 = parseFloat(prompt("Digite um número: "));
+let numero2 = parseFloat(prompt("Digite um número: "));
+let soma = numero1 + numero2;
+alert(numero1 + " + " + numero2 + " = " + soma);
